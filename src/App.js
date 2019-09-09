5694f8d5b89602185288eee555c833cb00c6e237
Roman_v <vigmol@ukr.net>
2019-09-09 22:14:33 +0300

    add key create class App create shopping list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            buyItem: ['milk', 'bread', 'fruit']
+        }
+    }
+
+    render() {
+        const {buyItem}=this.state;
+        return <div>
+            <h1>Shopping list</h1>
+            {buyItem.map(item => {
+                    return <p key={item}>{item}</p>
+                }
+            )}
+        </div>
+    }
 }
 
-export default App;
