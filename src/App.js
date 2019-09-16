6b48f3c9264fed0fecc1b3ab3651ac75b5723da9
Roman_v <vigmol@ukr.net>
2019-09-16 22:32:58 +0300

    lesson 4 add list product

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import myLogo from './App.css'
+import myLogo from './img/logo.png';
+import './App.css'
 
 
 export class App extends React.Component {
@@ -9,22 +10,65 @@ export class App extends React.Component {
         this.state = {
             buyItem: ['milk', 'bread', 'fruit']
         }
+
     }
+
+    addItem(e){
+        e.preventDefault();
+        const {buyItem}=this.state;
+        const newItem=this.newItem.value;
+
+        this.setState({
+            buyItem:[...this.state.buyItem,newItem]
+        });
+        this.addForm.reset();
+    }
+
 
     render() {
         const {buyItem} = this.state;
-        return <div>
-            <h1>Shopping list</h1>
-            <header className={myLogo.test}>
+
+        return (
+        <div className="container-item">
+            <h1 className="title">Shopping list</h1>
+            <header className="header">
                 <img src={myLogo} alt="logo"/>
             </header>
+            <form  ref ={input=>this.addForm=input} className="form-inline" onSubmit={(e)=>this.addItem(e)}>
+                <div className="form-group">
+                    <label className="sr-only" htmlFor="newItemInput">Add New Item</label>
+                    <input ref ={input=>this.newItem=input} type="text" placeholder="Bread" className="form-control" id="newItemInput"/>
+                    <div>
+                        <button type="submit" className="btn btn-primary">Add</button>
+                    </div>
+                </div>
+            </form>
             <div className="container">
-                {buyItem.map(item => {
-                        return <p key={item}>{item}</p>
-                    }
-                )}
+            <caption className="min-vw-100">Shopping list</caption>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Item</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {buyItem.map(item => {
+                            return (
+                                <tr key={item}>
+                                    <th scope="row">1</th>
+                                    <td>{item}</td>
+                                    <td>Button</td>
+                                </tr>)
+                        }
+                    )}
+                    </tbody>
+                </table>
+
             </div>
         </div>
+        )
     }
 }
 
